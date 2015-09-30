2eb90aea09e3498a640404e6472af167a85422cc
tannerkrewson <tannerkrewson@gmail.com>
2015-09-29 20:36:38 -0500

    Made board drawing more modular

--- src/connect4fx/Board.java
+++ src/connect4fx/Board.java
@@ -1,51 +1,70 @@
 package connect4fx;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Board {
 
     private GraphicsContext gc;
-    private int chipSize;
+    private int chipDiam;
+    private double canvasX, canvasY;
     private BoardSpace[][] board = new BoardSpace[7][6];
 
     public Board(int cs) {
-        chipSize = cs;
+        chipDiam = cs;
     }
 
     public Board(int cs, GraphicsContext b) {
-        chipSize = cs;
+        chipDiam = cs;
         gc = b;
+        Canvas temp = gc.getCanvas();
+        canvasX = temp.getWidth();
+        canvasY = temp.getHeight();
     }
 
     public void drawBoard() {
         //the instance of Board should have been created with a GraphicsContext
         //if we're going to be able to draw it.
         assert gc != null;
+        
+        //because we're assuming the chip boxes are sqaures
+        assert canvasX == canvasY;
+        
+        //width of a single square on the board
+         double sw = canvasX / 7;
 
         //yellow part of board
         gc.setFill(Color.YELLOW);
-        gc.fillRect(0, 100, 700, 600);
+        //leave room for hovering chips
+        gc.fillRect(0, sw, canvasX, canvasY - sw);
 
         //white circles
         gc.setFill(Color.WHITE);
-        double cpm = chipPosModifier(100, chipSize);
+        double cpm = chipPosModifier(100, chipDiam);
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 6; j++) {
-                gc.fillOval((i * 100) + cpm, (j * 100) + 100 + cpm, chipSize, chipSize);
+                gc.fillOval(getChipXPos(i), getChipYPos(j), chipDiam, chipDiam);
             }
         }
     }
 
     public boolean dropChip(int x, int y, Color c) {
-        Chip tempChip = new Chip(x, y, Color.BLUE);
-        tempChip.drawChip(gc, chipPosModifier(100, chipSize));
+        Chip tempChip = new Chip(x, y, c);
+        tempChip.drawChip(gc, chipPosModifier(100, chipDiam));
         board[x][y] = tempChip;
         return true;
     }
 
-    private double chipPosModifier(int length, int diameter) {
+    private double chipPosModifier(double length, int diameter) {
         return (length - diameter) / 2;
     }
+    
+    private double getChipXPos(int column){
+        return (column * canvasX / 7) + chipPosModifier(canvasX / 7, chipDiam);
+    }
 
+    private double getChipYPos(int row){
+        return (row * canvasY / 7) + chipPosModifier(canvasY / 7, chipDiam) + (canvasY / 7);
+    }
 }
